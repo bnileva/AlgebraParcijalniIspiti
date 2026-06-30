66b7211f6c0d3ad8b54e978b20d7bc8193b2c81c
Avelin Bingula <abingula@dspace.hr>
2026-06-30 18:28:22 +0200

    Osnovno za ispit

---
+++ aspnet_core_unit_1A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet_core_unit_1A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
